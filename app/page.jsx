12b217239475a1c9289974dff7b81552c4a76578
mkamran093 <mkamran.bscssef20@iba-suk.edu.pk>
2023-12-11 16:21:32 +0500

    first commit

---
+++ app/page.jsx
@@ -0,0 +1,22 @@
+import Navbar from "@/components/Navbar"
+
+const style = {
+  wrapper: `h-screen w-screen flex flex-col`
+}
+
+export default function Home() {
+  return (
+    <div className={style.wrapper}>
+      <Navbar />
+      <div className={style.main}>
+        {/* map */}
+      </div>
+      <div className={style.rideRequestContainer}>
+        <div className={style.rideRequest}>
+          {/* location selector */}
+          {/* confirm ride */}
+        </div>
+      </div>
+    </div>
+  )
+}
